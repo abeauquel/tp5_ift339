8c6469c57395775b60c1269b398c28fdc3df3db1
abeauquel <beauquel.alexandre@gmail.com>
2019-11-28 18:43:05 -0500

    first commit (erreur compilation)

---
+++ cmake-build-debug/CMakeFiles/CodeDepart.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CodeDepart.dir/main.cpp.obj"
+  "CodeDepart.exe"
+  "CodeDepart.exe.manifest"
+  "CodeDepart.pdb"
+  "libCodeDepart.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CodeDepart.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,20 @@
+/**
+* \ file main.cpp
+* \ author Alexandre Beauquel
+* \ brief Ce fichier contient des tests des opérations
+*/
+
+
+#include "map.h"
+
+
+int main(){
+
+    //Test du code
+    cout << "Test du main " << endl ;
+
+    return 0;
+
+}
+
+
